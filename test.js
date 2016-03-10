082552448fbd806a6ab720d29242bba9e4da1f7b
James Monger <jameskmonger@hotmail.co.uk>
2016-03-10 00:10:01 +0000

    Fix tests
    Add commas
    Switch expected with result
    Stringify for comparison

--- test.js
+++ test.js
@@ -3,27 +3,27 @@ var splitkeep = require('./index.js')
 
 test('splits correctly when splitting on dot', function (t, input, output) {
     var result = splitkeep(input, '.');
-    t.assert.equal(result, output);
+    t.assert.equal(output.toString(), result.toString());
 }, [
     ['a.b', ['a.', 'b']],
     ['b.c', ['b.', 'c']],
-    ['hello-there.hi.' ['hello-there.', 'hi.']]
+    ['hello-there.hi.', ['hello-there.', 'hi.', '']]
 ]);
 
 test('splits correctly when splitting on new line', function (t, input, output) {
     var result = splitkeep(input, '\n');
-    t.assert.equal(result, output);
+    t.assert.equal(output.toString(), result.toString());
 }, [
     ['a\nb', ['a\n', 'b']],
     ['b\nc', ['b\n', 'c']],
-    ['hello-there\nhi\n' ['hello-there\n', 'hi\n']]
+    ['hello-there\nhi\n', ['hello-there\n', 'hi\n', '']]
 ]);
 
 test('splits correctly when splitting on ampersand', function (t, input, output) {
     var result = splitkeep(input, '&');
-    t.assert.equal(result, output);
+    t.assert.equal(output.toString(), result.toString());
 }, [
     ['a&b', ['a&', 'b']],
     ['b&c', ['b&', 'c']],
-    ['hello-there&hi&' ['hello-there&', 'hi&']]
+    ['hello-there&hi&', ['hello-there&', 'hi&', '']]
 ]);
