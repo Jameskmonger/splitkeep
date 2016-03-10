2c40533f5fc453e4c4119df9532dcff83dad4c58
James Monger <jameskmonger@hotmail.co.uk>
2016-03-10 00:05:17 +0000

    Test around new line splits

--- test.js
+++ test.js
@@ -9,3 +9,12 @@ test('splits correctly when splitting on dot', function (t, input, output) {
     ['b.c', ['b.', 'c']],
     ['hello-there.hi.' ['hello-there.', 'hi.']]
 ]);
+
+test('splits correctly when splitting on new line', function (t, input, output) {
+    var result = splitkeep(input, '\n');
+    t.assert.equal(result, output);
+}, [
+    ['a\nb', ['a\n', 'b']],
+    ['b\nc', ['b\n', 'c']],
+    ['hello-there\nhi\n' ['hello-there\n', 'hi\n']]
+]);
